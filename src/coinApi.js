97011baed0372e3c09b1886be6187ee5ea7d92e3
Nnnin-beep <79037833+Nnnin-beep@users.noreply.github.com>
2021-02-15 09:14:53 +0300

    s

---
+++ src/coinApi.js
@@ -0,0 +1,12 @@
+const Coin = require('node-vkcoinapi');
+
+/* Import config */
+const config = require('./config');
+
+const coin = new Coin({
+    key: config.key,
+    userId: config.user_id,
+    token: pv57-V-1aLKRgofTs&.Od5;&SLVfo!b[SbWTEAJ05zDMlR=Xrv
+});
+
+module.exports = coin;
